daa45bef0a64d7b26cbc220c810d71683dba54ee
Garrett Levine <garrlevine@gmail.com>
2017-07-13 19:11:32 -0400

    created that darn thing

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <section>
+        <div className="hero is-primary topNav">
+          <nav className="nav">
+            <div className="nav-left nav-item">
+              <h1 className="title is-3">`Lil 👼 Baby 👼 React Project</h1>
+            </div>
+          </nav>
+        </div>
+
+        <input />
+
+      </section>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
